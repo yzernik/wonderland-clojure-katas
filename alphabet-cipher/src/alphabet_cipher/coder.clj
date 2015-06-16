e7b9d6fb7680769ab60023b4c60ff8b325b5be0d
yzernik <yzernik@gmail.com>
2015-06-15 22:32:17 -0700

    fixed function name convention

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,30 +1,30 @@
 (ns alphabet-cipher.coder)
 
-(defn to_int [c]
+(defn to-int [c]
   (- (int c) (int \a)))
 
-(defn from_int [n]
+(defn from-int [n]
   (char (+ (int \a) n)))  
 
-(defn get_offset [kw i]
+(defn get-offset [kw i]
   (let [j (mod i (count kw))]
-    (to_int (.charAt kw j))))
+    (to-int (.charAt kw j))))
 
-(defn shift_char [c offset]
-  (let [s (+ offset (to_int c))
-        r (+ 1 (to_int \z))]
-    (from_int (mod s r))))
+(defn shift-char [c offset]
+  (let [s (+ offset (to-int c))
+        r (+ 1 (to-int \z))]
+    (from-int (mod s r))))
 
-(defn encode_char [kw]
+(defn encode-char [kw]
   (fn [i c]
-    (shift_char c (get_offset kw i))))
+    (shift-char c (get-offset kw i))))
 
-(defn decode_char [kw]
+(defn decode-char [kw]
   (fn [i c]
-    (shift_char c (- (get_offset kw i)))))
+    (shift-char c (- (get-offset kw i)))))
 
 (defn encode [keyword message]
-  (apply str (map-indexed (encode_char keyword) message)))
+  (apply str (map-indexed (encode-char keyword) message)))
 
 (defn decode [keyword message]
-  (apply str (map-indexed (decode_char keyword) message)))
+  (apply str (map-indexed (decode-char keyword) message)))
