9decc82eed3934427cad75f5bd520af805ed2c7c
Jonathan Zernik <yzernik@gmail.com>
2015-06-16 20:59:39 -0700

    solved card game war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,36 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn suit [card]
+  (get card 0))
 
-(defn play-game [player1-cards player2-cards])
+(defn rank [card]
+  (get card 1))
+
+(defn make-comparator [order]
+  (fn [v1 v2]
+    (- (.indexOf order v1)
+       (.indexOf order v2))))
+
+(defn compare-suits [c1 c2]
+  ((make-comparator suits) (suit c1) (suit c2)))
+
+(defn compare-ranks [c1 c2]
+  ((make-comparator ranks) (rank c1) (rank c2)))
+
+(defn play-round [c1 c2]
+  (let [rank-diff (compare-ranks c1 c2)
+        suit-diff (compare-suits c1 c2)]
+    (or (> rank-diff 0)
+        (and (= rank-diff 0)
+             (> suit-diff 0)))))
+
+(defn play-game [cards1 cards2]
+  (cond (empty? cards1) false
+        (empty? cards2) true
+        :else (let [[c1 & hand1] cards1
+                    [c2 & hand2] cards2]
+                (cond (play-round c1 c2)
+                      (recur (into hand1 [c1 c2]) hand2)
+                      :else
+                      (recur hand1 (into hand2 [c1 c2]))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,46 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
+(defn beats [card1 card2]
+  (is (play-round card1 card2))
+  (is (not (play-round card2 card1))))
 
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (doseq [[r1 r2] (partition 2 1 ranks)
+            s1 suits
+            s2 suits]
+      (beats [s1 r2] [s2 r1])))
+  (testing "queens are higher rank than jacks"
+    (doseq [s1 suits
+            s2 suits]
+      (beats [s1 :queen] [s2 :jack])))
+  (testing "kings are higher rank than queens"
+    (doseq [s1 suits
+            s2 suits]
+      (beats [s1 :king] [s2 :queen])))
+  (testing "aces are higher rank than kings"
+    (doseq [s1 suits
+            s2 suits]
+      (beats [s1 :ace] [s2 :king])))
+  (testing "if the ranks are equal, clubs beat spades"
+    (doseq [r ranks]
+      (beats [:club r] [:spade r])))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (doseq [r ranks]
+      (beats [:diamond r] [:club r])))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (doseq [r ranks]
+      (beats [:heart r] [:diamond r]))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "the player loses when they run out of cards"
+    (is (not (play-game nil '([:spade 5]))))
+    (is (play-game '([:spade 5]) nil))
+    (is (play-game '([:spade :queen]) '([:heart 2]
+                                        [:heart 3]
+                                        [:heart 4]
+                                        [:heart 5]
+                                        [:heart 6]
+                                        [:heart 7])))))
