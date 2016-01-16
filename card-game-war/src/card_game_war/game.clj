410c96b548f34d528dcfb6a1e453140a6043ef70
yzernik <yzernik@gmail.com>
2016-01-15 23:31:20 -0800

    Improved card game solution

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -3,42 +3,36 @@
 ;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
+
 (def cards
   (for [suit suits
         rank ranks]
-    [suit rank]))
+    {:suit suit :rank rank}))
 
-(defn suit [card]
-  (get card 0))
-
-(defn rank [card]
-  (get card 1))
-
-(defn make-comparator [prop order]
-  (let [get-pos #(.indexOf order (prop %))]
+(defn make-comparator [key order]
+  (let [get-pos #(.indexOf order (key %))]
     (fn [c1 c2]
       (- (get-pos c1)
          (get-pos c2)))))
 
 (def compare-suits
-  (make-comparator suit suits))
+  (make-comparator :suit suits))
 
 (def compare-ranks
-  (make-comparator rank ranks))
+  (make-comparator :rank ranks))
 
 (defn play-round [c1 c2]
   (let [rank-diff (compare-ranks c1 c2)
         suit-diff (compare-suits c1 c2)]
     (or (> rank-diff 0)
         (and (= rank-diff 0)
-             (> suit-diff 0)))))
+             (< suit-diff 0)))))
 
 (defn play-game [cards1 cards2]
   (cond (empty? cards1) false
         (empty? cards2) true
         :else (let [[c1 & hand1] cards1
                     [c2 & hand2] cards2]
-                (cond (play-round c1 c2)
-                      (recur (into hand1 [c1 c2]) hand2)
-                      :else
-                      (recur hand1 (into hand2 [c1 c2]))))))
+                (if (play-round c1 c2)
+                  (recur (into hand1 [c1 c2]) hand2)
+                  (recur hand1 (into hand2 [c1 c2]))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,46 +2,37 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
+(defn make-card [suit rank]
+  {:suit suit :rank rank})
+
 (defn beats [card1 card2]
   (is (play-round card1 card2))
   (is (not (play-round card2 card1))))
 
 ;; fill in  tests for your game
 (deftest test-play-round
+  (testing "the highest rank wins the cards in the round")
+  (beats (make-card :diamond :ace)
+         (make-card :heart :ace)))
+
+(deftest test-play-round
   (testing "the highest rank wins the cards in the round"
     (doseq [[r1 r2] (partition 2 1 ranks)
             s1 suits
             s2 suits]
-      (beats [s1 r2] [s2 r1])))
-  (testing "queens are higher rank than jacks"
-    (doseq [s1 suits
-            s2 suits]
-      (beats [s1 :queen] [s2 :jack])))
-  (testing "kings are higher rank than queens"
-    (doseq [s1 suits
-            s2 suits]
-      (beats [s1 :king] [s2 :queen])))
-  (testing "aces are higher rank than kings"
-    (doseq [s1 suits
-            s2 suits]
-      (beats [s1 :ace] [s2 :king])))
-  (testing "if the ranks are equal, clubs beat spades"
-    (doseq [r ranks]
-      (beats [:club r] [:spade r])))
-  (testing "if the ranks are equal, diamonds beat clubs"
-    (doseq [r ranks]
-      (beats [:diamond r] [:club r])))
-  (testing "if the ranks are equal, hearts beat diamonds"
-    (doseq [r ranks]
-      (beats [:heart r] [:diamond r]))))
+      (beats (make-card s1 r2) (make-card s2 r1))))
+  (testing "if the ranks are equal, the higher suit wins"
+    (doseq [r ranks
+            [s1 s2] (partition 2 1 suits)]
+      (beats (make-card s1 r) (make-card s2 r)))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"
-    (is (not (play-game nil '([:spade 5]))))
-    (is (play-game '([:spade 5]) nil))
-    (is (play-game '([:spade :queen]) '([:heart 2]
-                                        [:heart 3]
-                                        [:heart 4]
-                                        [:heart 5]
-                                        [:heart 6]
-                                        [:heart 7])))))
+  (testing "the player loses when they have no cards"
+    (let [five-of-spades (make-card :spade 5)]
+      (is (not (play-game nil [five-of-spades])))
+      (is (play-game [five-of-spades] nil))))
+  (testing "the player loses when they have no cards"
+    (let [queen-of-diamonds (make-card :diamond :queen)]
+      (is (play-game [queen-of-diamonds]
+                     (for [i (range 2 9)]
+                       (make-card :spade i)))))))
